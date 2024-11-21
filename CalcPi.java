89527979e193fa08ffd685ac27dce0a069a097d6
miryam100 <miryam.marcus@post.runi.ac.il>
2024-11-22 00:35:33 +0200

    initial commit - salution Hw2

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,17 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int numTerms = Integer.parseInt(args[0]);
+		double calc = 0.0;
+		for (int i=0; i<numTerms; i++){
+			double fraction = 1.0/((2*i)+1);
+			if (i%2==0){
+				calc += fraction;
+			} else {
+				calc -= fraction;
+			}
+		}
+		System.out.println("pi according to Java: "+Math.PI);
+		System.out.println("pi, approximated:     "+(calc*4));
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,20 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int originalHours = Integer.parseInt(""+args[0].charAt(0)+args[0].charAt(1));
+        int originalMinutes = Integer.parseInt(""+args[0].charAt(3)+args[0].charAt(4));
+        int minutesAdd = Integer.parseInt(args[1]);
+        int totalMinutes = (originalHours * 60) + originalMinutes + minutesAdd;
+        int totalHours = totalMinutes / 60;
+        int newHour = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours*60);
+        if (newHour<10){
+            System.out.print ("0"+newHour+":");
+        } else {
+            System.out.print (newHour+":");}
+        if (newMinutes<10){
+            System.out.println("0"+newMinutes);
+        } else {    
+            System.out.println(newMinutes);
+        }            
     }
 }
